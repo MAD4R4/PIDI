9f09cf36f01c683abb3752e1017ee2fd7a7dfb15
jsjeferson <56982748+jsjeferson@users.noreply.github.com>
2019-10-24 20:17:12 -0300

    Colocado display no modelo categoria

--- PIDI/Models/Admin/CategoryModel.cs
+++ PIDI/Models/Admin/CategoryModel.cs
@@ -2,6 +2,7 @@
 using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,9 +14,12 @@ namespace PIDI.Models
         public ObjectId Id { get; set; }
 
         [BsonElement("CategoryName")]
+        [Display(Name ="Nome da Categoria")]
         public string CategoryName { get; set; }
 
         [BsonElement("CategoryDescription")]
+        [Display(Name = "Descrição")]
+
         public string CategoryDescription { get; set; }
     }
 }
