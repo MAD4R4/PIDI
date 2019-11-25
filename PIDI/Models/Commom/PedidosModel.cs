30ca1ede4fc196a3dbed813ddc8cec30cd4c49fd
jsjeferson <56982748+jsjeferson@users.noreply.github.com>
2019-11-25 00:35:03 -0300

    Front Carrinho-admin

--- PIDI/Models/Commom/PedidosModel.cs
+++ PIDI/Models/Commom/PedidosModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -12,28 +13,37 @@ namespace PIDI.Models.Commom
     public class PedidosModel
     {
         [BsonId]
+        [BsonElement("OrderId")]
+        [Display(Name = "ID do Pedido")]
         public ObjectId OrderId { get; set; }
-
+        [BsonElement("OrderDate")]
+        [Display(Name = "Data do Pedido")]
         public System.DateTime OrderDate { get; set; }
-
+        [BsonElement("userId")]
+        [Display(Name = "ID do usuário")]
         public string userId { get; set; }
-
+        [BsonElement("Address")]
+        [Display(Name = "Endereço")]
         public string Address { get; set; }
-
+        [BsonElement("City")]
+        [Display(Name = "Cidade")]
         public string City { get; set; }
-
+        [BsonElement("State")]
+        [Display(Name = "UF")]
         public string State { get; set; }
-
+        [BsonElement("PostalCode")]
+        [Display(Name = "CEP")]
         public string PostalCode { get; set; }
-
+        [BsonElement("Country")]
+        [Display(Name = "País")]
         public string Country { get; set; }
-
+        [BsonElement("produtosRequisitados")]
+        [Display(Name = "Produtos")]
         public List<PedidoElementModel> produtosRequisitados { get; set; }
-
         public decimal Total { get; set; }
-
         public string paymentType { get; set; }
-
+        [BsonElement("orderState")]
+        [Display(Name = "Status do Pedido")]
         public string orderState { get; set; }
 
     }
